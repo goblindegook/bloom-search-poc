299b8112b80dfc8bbbaed821c1c3e951d828c5c4
Luis Rodrigues <lrodrigues@newstore.com>
2023-05-29 11:06:36 +0200

    Bloom filter visualization

---
+++ src/bloom-filter/main.tsx
@@ -0,0 +1,164 @@
+import './style.css'
+import React from 'preact/compat'
+import { render } from 'preact'
+import { signal } from '@preact/signals'
+import { hashLocations } from '@pacote/bloom-filter/src/hash'
+import cx from 'classnames'
+
+const n = signal<number>(100)
+const hashes = signal<number>(3)
+const words = signal<string[]>([])
+const filter = signal<boolean[]>(Array(n.value).fill(false))
+const added = signal<{ word: string; hashes: number[] }>({
+  word: '',
+  hashes: [],
+})
+const highlighted = signal<{ word: string; hashes: number[] }>({
+  word: '',
+  hashes: [],
+})
+const searched = signal<{ word: string; hashes: number[] }>({
+  word: '',
+  hashes: [],
+})
+
+function App() {
+  const computeHashes = hashLocations(n.value, hashes.value, 0)
+
+  const reset = () => {
+    words.value = []
+    filter.value = Array(n.value).fill(false)
+    added.value = { word: '', hashes: [] }
+    highlighted.value = { word: '', hashes: [] }
+  }
+
+  return (
+    <div>
+      <div>
+        <label>
+          Size
+          <input
+            onChange={(event: any) => {
+              n.value = Number(event.target.value)
+              reset()
+            }}
+            value={n}
+            type="number"
+          />
+        </label>
+        <label>
+          Hashes
+          <input
+            onChange={(event: any) => {
+              hashes.value = Number(event.target.value)
+              reset()
+            }}
+            value={hashes}
+            type="number"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Add
+          <input
+            onKeyUp={(event: any) => {
+              if (event.key === 'Enter') {
+                const word = event.target.value
+                const locations = computeHashes(word)
+                words.value = words.value.concat(word)
+                locations.forEach((location) => {
+                  filter.value[location] = true
+                })
+                added.value = { word, hashes: locations }
+                event.target.value = ''
+              }
+            }}
+          />
+        </label>
+        <div class="last-word">
+          <div>{added.value.word}</div>
+          <ul class="added-hashes">
+            {added.value.hashes.map((value) => (
+              <li>{value}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div class="added-words">
+        {words.value.map((word) => (
+          <div
+            class={cx('added-word', {
+              highlighted: word === highlighted.value.word,
+            })}
+          >
+            <button
+              onClick={(event) => {
+                if (word === highlighted.value.word) {
+                  highlighted.value = { word: '', hashes: [] }
+                } else {
+                  highlighted.value = { word, hashes: computeHashes(word) }
+                }
+                event.preventDefault()
+              }}
+            >
+              {word}
+            </button>
+            {/* <button
+              onClick={(event) => {
+                words.value = words.value
+                  .slice(0, index)
+                  .concat(words.value.slice(index + 1, words.value.length))
+                event.preventDefault()
+              }}
+            >
+              X
+            </button> */}
+          </div>
+        ))}
+      </div>
+      <ul class="filter">
+        {filter.value.map((value, index) => (
+          <li
+            class={cx({
+              location: true,
+              on: value,
+              off: !value,
+              highlighted: highlighted.value.hashes.includes(index),
+              added: added.value.hashes.includes(index),
+              searched:
+                searched.value.hashes.length > 0 &&
+                searched.value.hashes.includes(index),
+            })}
+          >
+            <span class="index">{index}</span>
+          </li>
+        ))}
+      </ul>
+
+      <label>
+        Search
+        <input
+          onKeyUp={(event: any) => {
+            const word = event.target.value
+            if (word.length) {
+              searched.value = { word, hashes: computeHashes(word) }
+            } else {
+              searched.value = { word, hashes: [] }
+            }
+          }}
+        />
+      </label>
+
+      {searched.value.word !== '' && (
+        <div>
+          {searched.value.hashes.every((index) => filter.value[index])
+            ? 'Probably there.'
+            : 'Definitely not there.'}
+        </div>
+      )}
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root')!)
