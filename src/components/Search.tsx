458f804b44f340aa1a979fae5240abb2f87b912e
Luis Rodrigues <lrodrigues@newstore.com>
2023-06-20 21:13:11 +0200

    update

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,19 +14,19 @@ export const Search = ({ label, children, ...props }: SearchProps) => {
       </label>
       <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg
-          aria-hidden="true"
-          class="w-5 h-5 text-gray-500"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-search w-5 h-5 text-gray-500"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          ></path>
+          <circle cx="11" cy="11" r="8" />
+          <path d="m21 21-4.3-4.3" />
         </svg>
       </div>
       <input
